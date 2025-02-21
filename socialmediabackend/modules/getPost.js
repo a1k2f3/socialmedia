fcfb8cddf6054b700c9a953eb92b6d0238506f26
akif <su92-bssem-f22-118@superior.edu.pk>
2025-02-21 11:47:22 +0500

    jo

--- socialmediabackend/modules/getPost.js
+++ socialmediabackend/modules/getPost.js
@@ -2,14 +2,15 @@ import express from "express";
 import Post from "../Schema/Post.js";
 const router = express.Router();
 // Route to handle post creation
-router.get("/post",async (req, res) => {
+router.get("/post", async (req, res) => {
   try {
-    const posts= await Post.find();
-    res.status(201).json(posts);
+    const posts = await Post.find();
+    res.status(200).json(posts);
   } catch (error) {
-    console.error("Error creating post:", error);
+    console.error("Error fetching posts:", error);
     res.status(500).json({ message: "Server error" });
   }
 });
+
 
 export default router;
