4d90715bd0bc58b80b33b83a8347e522ea1cd479
akif <su92-bssem-f22-118@superior.edu.pk>
2025-02-24 23:16:53 +0500

    add server

--- socialmediabackend/server.js
+++ socialmediabackend/server.js
@@ -7,7 +7,6 @@ import commentRoutes from "./modules/Comment.js";
 import findUserRoutes from "./modules/finduser.js";
 import findPostRoutes from "./modules/getPost.js";
 import fetchCommentRoutes from "./modules/fetchcomment.js";
-import search from "./modules/searchdata.js";
 import suggestionRoutes from "./modules/suggest.js";
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -40,7 +39,6 @@ app.use("/api", suggestionRoutes);
 app.use("/api", findPostRoutes,express.static('public/media'));
 app.use("/api", fetchCommentRoutes);
 app.use("/api", acounts);
-app.use("/api", search);
 io.on("connection", (socket) => {
   console.log("User connected");
   socket.on("disconnect", () => {
